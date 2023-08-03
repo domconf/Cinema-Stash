8e92979ff45e04501f999fd6d7cb9e699bb26009
Dominick Confessore <domconfessore@gmail.com>
2023-08-03 10:25:54 -0400

    partially fixed issue within saved movies tab

--- client/src/pages/SavedMovies.js
+++ client/src/pages/SavedMovies.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 import Auth from '../utils/auth';
+import { removeMovieId } from '../utils/localStorage';
+import { useQuery, useMutation } from '@apollo/client';
+import { GET_ME } from '../utils/queries';
+import { REMOVE_MOVIE } from '../utils/mutations';
 
-const SavedMovies = (props) => {
-    const savedMovies = undefined
-    const handleDeleteMovie = undefined
-    if (!savedMovies || savedMovies.length === 0) {
-        return (
-            <>
-                <Jumbotron fluid className='text-light bg-dark'>
-                    <Container>
-                        <h1>Viewing saved movies!</h1>
-                    </Container>
-                </Jumbotron>
-                <Container>
-                    <h2>You have no saved movies!</h2>
-                </Container>
-            </>
-        );
+const SavedMovies = () => {
+    const { loading, data } = useQuery(GET_ME);
+    const [removeMovie, { error }] = useMutation(REMOVE_MOVIE);
+    const userData = data?.me || {};
+
+    const handleDeleteMovie = async (movieId) => {
+        const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+        if (!token) {
+            return false;
+        }
+
+        try {
+            const { data } = await removeMovie({
+                variables: { movieId },
+            });
+
+            removeMovieId(movieId);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    if (loading) {
+        return <h2>LOADING...</h2>;
     }
 
     return (
@@ -29,15 +42,19 @@ const SavedMovies = (props) => {
             </Jumbotron>
             <Container>
                 <h2>
-                    {savedMovies.length
-                        ? `Viewing ${savedMovies.length} saved ${savedMovies.length === 1 ? 'movie' : 'movies'}:`
+                    {userData.savedMovies.length
+                        ? `Viewing ${userData.savedMovies.length} saved ${userData.savedMovies.length === 1 ? 'movie' : 'movies'}:`
                         : 'You have no saved movies!'}
                 </h2>
                 <CardColumns>
-                    {savedMovies.map((movie) => {
+                    {userData.savedMovies.map((movie) => {
                         return (
                             <Card key={movie.movieId} border='dark'>
-                                {movie.poster ? <Card.Img src={movie.poster} alt={`The cover for ${movie.title}`} variant='top' /> : null}
+                                {movie.poster ? (
+                                    <Card.Img src={movie.poster} alt={`The cover for ${movie.title}`} variant='top' />
+                                ) : (
+                                    <div>No Poster Available</div>
+                                )}
                                 <Card.Body>
                                     <Card.Title>{movie.title}</Card.Title>
                                     <p className='small'>Year: {movie.year}</p>
