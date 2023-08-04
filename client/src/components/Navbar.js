3738f6198b01153c0be2a4d3be011a17bd381bad
Dominick Confessore <domconfessore@gmail.com>
2023-08-04 19:51:45 -0400

    cleaned up code, removed unecessary notations

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,80 +1,3 @@
-// import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
-// import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
-// import SignUpForm from './SignupForm';
-// import LoginForm from './LoginForm';
-
-// import Auth from '../utils/auth';
-
-// const AppNavbar = () => {
-//     const [showModal, setShowModal] = useState(false);
-
-//     return (
-//         <>
-//             <Navbar style={{ backgroundColor: '#8B0000', fontFamily: 'Impact', color: 'white' }} expand='lg'>
-//                 <Container fluid>
-//                     <Navbar.Brand as={Link} to='/'>
-//                         Cinema Stash
-//                     </Navbar.Brand>
-//                     <Navbar.Toggle aria-controls='navbar' />
-//                     <Navbar.Collapse id='navbar'>
-//                         <Nav className='ml-auto'>
-//                             <Nav.Link as={Link} to='/'>
-//                                 Search Movies
-//                             </Nav.Link>
-//                             {/* if user is logged in show saved movies and logout */}
-//                             {Auth.loggedIn() ? (
-//                                 <>
-//                                     <Nav.Link as={Link} to='/saved'>
-//                                         Saved Movies
-//                                     </Nav.Link>
-//                                     <Nav.Link onClick={Auth.logout} style={{ color: 'black' }}>
-//                                         Logout
-//                                     </Nav.Link>
-//                                 </>
-//                             ) : (
-//                                 <Nav.Link onClick={() => setShowModal(true)} style={{ color: 'black' }}>
-//                                     Login/Sign Up
-//                                 </Nav.Link>
-//                             )}
-//                         </Nav>
-//                     </Navbar.Collapse>
-//                 </Container>
-//             </Navbar>
-//             {/* set modal data up */}
-//             <Modal size='lg' show={showModal} onHide={() => setShowModal(false)} aria-labelledby='signup-modal'>
-//                 {/* tab container to do either signup or login component */}
-//                 <Tab.Container defaultActiveKey='login'>
-//                     <Modal.Header closeButton>
-//                         <Modal.Title id='signup-modal'>
-//                             <Nav variant='pills'>
-//                                 <Nav.Item>
-//                                     <Nav.Link eventKey='login'>Login</Nav.Link>
-//                                 </Nav.Item>
-//                                 <Nav.Item>
-//                                     <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
-//                                 </Nav.Item>
-//                             </Nav>
-//                         </Modal.Title>
-//                     </Modal.Header>
-//                     <Modal.Body>
-//                         <Tab.Content>
-//                             <Tab.Pane eventKey='login'>
-//                                 <LoginForm handleModalClose={() => setShowModal(false)} />
-//                             </Tab.Pane>
-//                             <Tab.Pane eventKey='signup'>
-//                                 <SignUpForm handleModalClose={() => setShowModal(false)} />
-//                             </Tab.Pane>
-//                         </Tab.Content>
-//                     </Modal.Body>
-//                 </Tab.Container>
-//             </Modal>
-//         </>
-//     );
-// };
-
-// export default AppNavbar;
-
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
@@ -85,7 +8,7 @@ import Auth from '../utils/auth';
 
 const AppNavbar = () => {
     const [showModal, setShowModal] = useState(false);
-    const [activeTab, setActiveTab] = useState('login'); // Track the active tab
+    const [activeTab, setActiveTab] = useState('login')
 
     return (
         <>
@@ -100,7 +23,6 @@ const AppNavbar = () => {
                             <Nav.Link as={Link} to='/'>
                                 Search Movies
                             </Nav.Link>
-                            {/* if user is logged in show saved movies and logout */}
                             {Auth.loggedIn() ? (
                                 <>
                                     <Nav.Link as={Link} to='/saved'>
@@ -119,9 +41,7 @@ const AppNavbar = () => {
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-            {/* set modal data up */}
             <Modal size='lg' show={showModal} onHide={() => setShowModal(false)} aria-labelledby='signup-modal'>
-                {/* tab container to do either signup or login component */}
                 <Tab.Container defaultActiveKey='login'>
                     <Modal.Header closeButton>
                         <Modal.Title id='signup-modal'>
